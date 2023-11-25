d3dccb4a91b8854ea5733e04b1250dbb8f1a9ec4
wdodigital 01 <wdodigital01@wdodigitals-Mac-mini.local>
2023-11-25 09:54:34 +0400

    notification

--- app/dashboard/notifications/(components)/notification-component.tsx
+++ app/dashboard/notifications/(components)/notification-component.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/lib/utils'
 import { Notification } from '@prisma/client'
 import {format} from 'date-fns'
-import { CheckCheck } from 'lucide-react'
+import { AlertTriangle, CheckCheck, CheckCircle, Delete } from 'lucide-react'
 import Link from 'next/link'
 
 type Props = {
@@ -14,28 +14,28 @@ const NotificationComponent = ({notification}: Props) => {
     const formattedDate = format(new Date(notification.createdAt), 'EEE, MMM/d, HH:mm')
     const url = notification.type==='SERVICE'? `/dashboard/services/${notification.IdHolder}` :''
 
-    const themes :{[key:string ] :string } =  {
+    const themes :{[key:string ] :React.ReactElement } =  {
         
-      APPROVE: 'bg-green-500/20 text-green-500 border border-green-500',
-      REQUEST: 'bg-yellow-500/20 text-yellow-500 border border-yellow-500',
-      DELETE: 'bg-rose-500/20 text-rose-500 border border-rose-500',
+      APPROVE:<CheckCircle className="text-green-500 w-5 h-5 flex-shrink-0"/>,
+      REQUEST: <AlertTriangle className="text-yellow-500 w-5 h-5 flex-shrink-0"/>,
+      DELETE: <Delete className="text-rose-500 w-5 h-5 flex-shrink-0" />,
   }
+  const showCheck = ((notification.type==='ENTITY' || notification.type==='SERVICE' )&& notification.status !=='DELETE') || notification.type==='BOOKING'
 
   return (
-    <div className={cn("rounded-lg p-6  relative flex gap-4 items-center ",themes[notification.status!],notification.isRead && 'opacity-60')}>
-    {!notification.isRead ? <span className="top-1  right-1 absolute   text-xs">New</span> : <span className="top-1  right-1 absolute  text-xs"><CheckCheck className="h-4 w-4" /></span>}
-        <p className='text-sm '>{notification.message}</p>
-     
-       
-        <Link href={url} className='hover:underline text-blue-500 text-sm'>Check</Link>
-       
-        <p className='text-xs text-neutral-500 pt-3 absolute bottom-2 right-3'>{formattedDate}</p>
-    
-    
-    
+    <div className={cn("rounded-lg p-6  relative flex gap-4 items-center bg-slate-100 border",notification.isRead && 'opacity-60')}>
+         {!notification.isRead ? <span className="top-1  right-1 absolute text-neutral-500  text-xs">New</span> : <span className="top-1  right-1 absolute  text-xs"><CheckCheck className="h-4 w-4 text-neutral-500" /></span>}
+     {themes[notification.status!]} <p className={cn("text-sm text-neutral-700")}>{notification.message}</p>
 
+      {showCheck&&<Link href={url} className="hover:underline text-blue-500 text-sm ml-auto sm:mr-32 ">
+        Check
+      </Link>}
+
+      <p className="text-xs text-neutral-500 pt-3 absolute bottom-2 right-3">
+        {formattedDate}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default NotificationComponent
